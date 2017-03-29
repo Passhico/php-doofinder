c086ae674d0bb7f7167e3059c15f370870bb9e86
passh <passh@pascual>
2017-03-29 10:36:13 +0200

    pccomDooFinderApiclient() creado file.

---
+++ src/pccomDooFinderApiClient.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Clase Crud de Artículos usando la API de DooFinder
+ * 
+ * @see https://github.com/Passhico/php-doofinder
+ * @see https://jira.pccomponentes.com/browse/DES-114
+ * 
+ * Esto usa composer , so composer update & composer install . 
+ * 
+ * CREDENCIALES PARA EL ENTORNO DE PRUEBAS.
+ * Management API: 6c1b7adcf350fb42b22d3aefa8885607
+ * Search API: eu1-6f643cf3868bd6b8d521053bc44a452573db005a
+ */
+
+require_once dirname(__FILE__) . '/vendor/autoload.php'; 
+
+/**
+ * Description of pccomDooFinderApiClient
+ *
+ * @author passh
+ */
+class pccomDooFinderApiClient
+{
+	//put your code here
+}
