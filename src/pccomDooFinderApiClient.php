8b4e65a7241fd8f53309f7b6b959c6e5e8899ae0
passh <passh@pascual>
2017-03-29 13:13:19 +0200

    https://jira.pccomponentes.com/browse/DES-114 v0.1

--- src/pccomDooFinderApiClient.php
+++ src/pccomDooFinderApiClient.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Clase Crud de Artículos usando la API de DooFinder
  * 
@@ -7,19 +8,84 @@
  * 
  * Esto usa composer , so composer update & composer install . 
  * 
- * CREDENCIALES PARA EL ENTORNO DE PRUEBAS.
- * Management API: 6c1b7adcf350fb42b22d3aefa8885607
- * Search API: eu1-6f643cf3868bd6b8d521053bc44a452573db005a
+ * CREDENCIALES PARA EL ENTORNO DE PRUEBAS CUENTA JM : 
+ * Management API:eu1-100811e0f54526963193683549b9682878a83d35
+ * 
+ * CREDENCIALES PARA EL ENTORNO DE PRUEBAS CUENTA PASCUAL: 
+ * 
+ * 	Management API: eu1-79cdbca29415f714e4bddb283397927e008065b4
+ *  Search API:     f05af2c30ce3af68bf27577892d2ada4
+ * 
+ * 
  */
+require_once './../vendor/autoload.php';
+require_once './../t00lz/t00lz.php'; 
 
-require_once dirname(__FILE__) . '/vendor/autoload.php'; 
+use Doofinder\Api\Management\Client as ManagementClient;
 
 /**
- * Description of pccomDooFinderApiClient
- *
- * @author passh
+ * Cliente para CRUD de los items del feeder usando Api de Doofinder.
  */
-class pccomDooFinderApiClient
+class pccomDooFinderApiClient extends ManagementClient
 {
-	//put your code here
+	/**
+	 * El motor actual.
+	 * @var \Doofinder\Api\Management\SearchEngine
+	 */
+	private $searchEngine; 
+	
+	/**
+	 * 
+	 * @param string $hashId El hash del motor. 
+	 * @param string $apiKey La Management Apikey
+	 */
+	public function __construct($apiKey)
+	{
+		parent::__construct($apiKey); 
+		
+	
+	}
+	
+	
+	function seleccionaMotor($motor)
+	{
+		foreach ($this->getSearchEngines() as $motor)
+		{
+			//var_dump($motor); 
+			t00lz::dump($motor); 
+		}
+	}
+
+	/**
+	 * Muestra los Engines disponibles y todos los datos asociados. 
+	 * Normalmente solo se usará para depuración, así podremos 
+	 * ver los nombres que tienen . 
+	 */
+	function showAvailiableSearchEngines()
+	{
+	
+		foreach ($this->getSearchEngines() as $motor)
+		{		
+		//	t00lz::dump($motor);
+		var_dump($motor); 
+		}
+	}
+	function setSearchEngine($NombreMotor)
+	{
+		$this->searchEngines = $this->getSearchEngines(); 
+	}
+	
 }
+
+$apiKeyMIA = 'eu1-79cdbca29415f714e4bddb283397927e008065b4'; 
+$apiKeyJM = 'eu1-100811e0f54526963193683549b9682878a83d35'; 
+
+$fooFinder = new pccomDooFinderApiClient($apiKeyJM) ; 
+$fooFinder->showMotores(); 
+
+
+
+
+
+
+
